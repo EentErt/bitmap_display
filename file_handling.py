1e896592a87ef86c829ff3d82f26c94681d646fa
EentErt <nate.giff@gmail.com>
2025-06-26 00:28:36 -0700

    finished

--- file_handling.py
+++ file_handling.py
@@ -4,6 +4,8 @@ def save_data(array, file_name = None):
     while True:
         if file_name is None:
             file_name = input("Enter file name to write to: ")
+            if file_name.upper() == "BACK":
+                break
         if os.path.isfile(file_name):
             print(f'File {file_name} already exists. If you would like to overwrite it, enter "overwrite"')
             user_input = input("Overwrite?: ")
--- interpret_input.py
+++ interpret_input.py
@@ -1,12 +1,18 @@
 from file_handling import save_data
 from enum import Enum
 
-Options = Enum("Option", ["HELP", "COMPRESS", "EXPAND", "FULL", "DISPLAY", "SAVE"])
+Options = Enum("Option", ["HELP", "COMPRESS", "EXPAND", "FULL"])
 LongOptions = Enum("LongOption", ["THRESHOLD", "HIGHLIGHT", "DISPLAY", "SAVE"])
 
-def test_input(data):
+#def test_input(data):
 
 def interpret(array, data):
+    if data.split(" ")[0].upper() in LongOptions.__members__:
+        try:
+            message = interpret_long(array, data.split(" "))
+            return message
+        except Exception as e:
+            return e
     match data.upper():
         case "HELP":
             with open("help.txt") as file:
@@ -16,96 +22,77 @@ def interpret(array, data):
             return None
         case "BACK":
             return None
-
         case "COMPRESS":
             try:
                 array.compress(1)
                 return "Image compressed"
             except Exception as e:
-                return e
-                
+                return e       
         case "EXPAND":
             try:
                 array.expand(1)
                 return "Image expanded"
             except Exception as e:
                 return e
-        
         case "FULL":
             try:
                 array.expand(0)
                 return "Image expanded to full size"
             except Exception as e:
                 return e
-
-        case "DISPLAY":
-            try:
-                message = change_display(array)
-                return message
-            except Exception as e:
-                return e
-
-        case "SAVE":
-            try:
-                message = save_data(array)
-                return message
-            except Exception as e:
-                return e
-
-    return interpret_long(array, data.split(" "))
-
+        case _:
+            return "Invalid command"
 
 
 def interpret_long(array, data):
     if len(data) > 2:
         raise ValueError("Invalid command. Please try again.")
-    if len(data) < 2:
-        print(f'Enter a valid {data[0].lower()} level, or enter "back" to cancel')
-        value = input("Enter value: ")
-        if value.upper() == "BACK":
-            return
-    else:
-        value = data[1]
-
-    if data[0].upper() == "THRESHOLD":
-        while True:
-            try:
-                array.set_threshold(value)
-                return f"Threshold set to {value}"
-            except Exception as e:
-                print(e)
-                print(f'Enter a valid threshold level, or enter "back" to cancel')
-                value = input("Enter value: ")
-                if value.upper() == "BACK":
-                    return
-                data[1] = value
-
-    if data[0].upper() == "HIGHLIGHT":
-        while True:
-            try:
-                array.set_highlight(value)
-                return f"Highlight level set to {value}"
-            except Exception as e:
-                print(e)
-                print(f'Enter a valid highlight level, or enter "back" to cancel')
-                value = input("Enter value: ")
-                if value.upper() == "BACK":
-                    return
-                data[1] = value
-
-    if data[0].upper() == "DISPLAY":
-        return change_display(array, data[1])
-
-    if data[0].upper() == "SAVE":
-        return save_data(array, data[1])
+    match data[0].upper():
+        case "THRESHOLD":
+            if len(data) == 1:
+                value = input("Enter a threshold value: ")
+                data.append(value)
+            while True:
+                try:
+                    array.set_threshold(data[-1])
+                    return f"Threshold set to {data[-1]}"
+                except Exception as e:
+                    print(e)
+                    print(f'Enter a valid threshold level, or enter "back" to cancel')
+                    value = input("Enter a threshold value: ")
+                    if value.upper() == "BACK":
+                        return
+                    data[1] = value
+        case "HIGHLIGHT":
+            if len(data) == 1:
+                value = input("Enter a highlight value: ")
+                data.append(value)
+            while True:
+                try:
+                    array.set_highlight(data[-1])
+                    return f"Highlight level set to {data[-1]}"
+                except Exception as e:
+                    print(e)
+                    print(f'Enter a valid highlight level, or enter "back" to cancel')
+                    value = input("Enter value: ")
+                    if value.upper() == "BACK":
+                        return
+                    data[1] = value
+        case "DISPLAY":
+            if len(data) == 1:
+                return change_display(array)
+            return change_display(array, data[1])
+        case "SAVE":
+            if len(data) == 1:
+                return save_data(array)
+            return save_data(array, data[1])
 
 def change_display(array, mode = None):
     while True:
-        if mode is None or mode.upper() == "DISPLAY":
+        if mode is None:
             with open("display.txt") as file:
                 print(file.read())
             mode = input("Enter display mode: ")
-
         try:
             array.set_display_mode(mode.upper())
             return f'Display mode set to "{mode.lower()}"'
